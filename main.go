ad8012868583e9d855422e121fad25e644f5b313
Pavel Zaitsev <pavel@arslogi.ca>
2019-11-04 10:44:41 +0300

    wip

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 )
 import "fmt"
 import "net/http"
+
 func Exists(name string) bool {
 	if _, err := os.Stat(name); err != nil {
 		if os.IsNotExist(err) {
@@ -14,13 +15,13 @@ func Exists(name string) bool {
 	}
 	return true
 }
-func handler(w http.ResponseWriter, r *http.Request){
+func handler(w http.ResponseWriter, r *http.Request) {
 	f := r.URL.Path[1:]
-	if(r.Method == "POST"){
+	if r.Method == "POST" {
 		// read photo and store it
 		fmt.Printf("a POST request!\n")
 		a_file, header, _ := r.FormFile("file")
-		write_to,_ := os.OpenFile(header.Filename ,os.O_RDWR|os.O_CREATE, os.FileMode(0666))
+		write_to, _ := os.OpenFile(header.Filename, os.O_RDWR|os.O_CREATE, os.FileMode(0666))
 		fmt.Printf("filename upload: %s\nfile size:%d\n", header.Filename, header.Size)
 		data := make([]byte, header.Size)
 		n, err := a_file.Read(data)
@@ -35,11 +36,11 @@ func handler(w http.ResponseWriter, r *http.Request){
 		_ = write_to.Close()
 	} else {
 
-		if (Exists(f)) {
+		if Exists(f) {
 			// serve the file
 			file, err := os.OpenFile(f, 0, os.FileMode(0777))
 
-			if (err != nil) {
+			if err != nil {
 				os.Exit(-1)
 			}
 			stat, _ := file.Stat()
@@ -51,7 +52,7 @@ func handler(w http.ResponseWriter, r *http.Request){
 }
 
 func main() {
-	fmt.Println("vim-go")
+	fmt.Println("file-saver")
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
